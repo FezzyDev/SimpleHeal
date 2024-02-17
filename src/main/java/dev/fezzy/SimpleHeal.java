b9e78882e7ea879442e820ab21a05bc2692f9d03
FezzyDev <fezzymail@gmail.com>
2024-02-17 16:00:19 -0600

    Modify output jar name

--- src/main/java/dev/fezzy/SimpleHeal.java
+++ src/main/java/dev/fezzy/SimpleHeal.java
@@ -2,6 +2,7 @@ package dev.fezzy;
 
 import dev.fezzy.commands.FeedCommand;
 import dev.fezzy.commands.HealCommand;
+import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class SimpleHeal extends JavaPlugin {
@@ -16,6 +17,8 @@ public final class SimpleHeal extends JavaPlugin {
         new ConfigHandler(this);
         this.getCommand("Heal").setExecutor(new HealCommand());
         this.getCommand("Feed").setExecutor(new FeedCommand());
+
+        Bukkit.getLogger().info("SimpleHeal enabled successfully!");
     }
 
     public void onDisable() {
